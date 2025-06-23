1661f972c01625bbd61257f7b86894669af96620
Theo607 <mateusz@smuga.pl>
2025-06-23 22:39:04 +0200

    dhmtl

--- SEMESTR2/KP/Lista5-main/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src","l":"DrawableShape"},{"p":"src","l":"Figures"},{"p":"app","l":"Paint"},{"p":"src","l":"PaintCanvas"},{"p":"src","l":"PaintMenuBar"},{"p":"src","l":"SerializableShape"},{"p":"src","l":"Tool"}];updateSearchResults();
